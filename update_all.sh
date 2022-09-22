b224ed91b921d2a08ffda17c3a03618651642590
ThinkerOfThoughts <thinkerofthoughts42@gmail.com>
2022-09-22 14:03:02 -0400

    Upgrade now upgrades
    Modified script to handle (or at least try to) when packegs are kept
    back from upgrading (i.e. "The following packages have been kept
    back:"). If thats encoutnered it will now ask if you want to try
    indavidually installing them; if yes, it will parse the list of kept
    back packages into an array, and iterate over that array running 'sudo
    apt-get install *package name*' for all of them.

--- update_all.sh
+++ update_all.sh
@@ -15,30 +15,70 @@ if [[ $EUID > 0 ]]
   printf "OK\n\n";
 
 
-  printf "Removing old package list.\n"
+  printf "\nRemoving old package list.\n"
   apt clean
 
-  printf "Updating apt package list.\n"
+  printf "\nUpdating apt package list.\n"
   apt update
 
-  printf "Checking if updates are available.\n"
+  printf "\nChecking if updates are available.\n"
 
 updates='$(/usr/lib/update-notifier/apt-check --human-readable)'
 if [ ${updates:0:1} != 0 ]
 then {
       printf "Updates found. Installing.\n"
-      apt -y dist-upgrade
 
+      #output of the command is going into a variable first so it can be used later
+      output=$(apt-get -y dist-upgrade | tee /dev/tty)
       if [ $? -ne 0 ]
       then {
           printf "\nError installing updates. Aborting.\n";
           exit $retVal;
       } fi
+
+      #checking if packages have been kept back for some reason
+      if [[ $output == *"The following packages have been kept back"* ]]
+      then {
+        printf "\nPackages have been kept back and not upgrade, want to try indavidually installing them (this should give apt what it needs to upgrade them)?\n"
+        read -p "Yes/No? " -n 1 -r
+        echo #move to new line
+        if [[ ! $REPLY =~ ^[Yy]$ ]]
+        then {
+            printf "\nOkay\n";
+        }
+        else {
+              printf "\nParsing list of packages.\n"
+              declare -a packages_array=("")
+
+              packages_string="$(awk 'BEGIN {p=0}; /kept back:/{ p = 1 ; next }; /upgraded./ { p = 0; next; }; p { print }' <<< "$output")" #seperates out the list of kept back packages
+
+              IFS=' ' read -r -a packages_array <<< "$packages_string" #splitting string of packages to array
+
+
+              printf "\nPackages to upgrade:\n"
+              echo $packages_string
+
+              printf "\nAttempting to upgrade.\n\n"
+
+              for i in "${packages_array[@]}"
+              do
+                printf "\nUpgrading: $i\n"
+
+                output=$(sudo apt-get install $i | tee /dev/tty)
+                if [ $? -ne 0 ]
+                then {
+                  printf "\nError installing updates. Aborting.\n";
+                  exit $retVal;
+                } fi
+              done
+
+            } fi
+      } fi
   }
   else {
-    printf "No updates found.\n";
+    printf "\nNo updates found.\n";
   } fi
 
-printf "Removing auto-installed packages that aren't needed any more.\n";
+printf "\nRemoving auto-installed packages that aren't needed any more.\n";
 #cleaning up
 apt -V autoremove
